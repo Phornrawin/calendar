8969885ecd4f8a974213dd62fd7837b1d4618d89
WEELT5602DZO721\Phornrawin <phornrawin.c@ku.th>
2017-08-29 23:50:07 +0700

    commit in paolo kaset commit before sleep - สร้าง addview เพิ่งขึ้นมา และ ทำหน้าต่างเสร็จแล้ว

---
+++ src/main/java/view/AddEventView.java
@@ -0,0 +1,18 @@
+package view;/**
+ * Created by Phornrawin on 29/8/2560.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AddEventView extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
