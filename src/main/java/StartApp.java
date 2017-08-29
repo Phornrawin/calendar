eac2e47fc55a9ee8e77a542ea092991090d0697e
WEELT5602DZO721\Phornrawin <phornrawin.c@ku.th>
2017-08-29 12:01:27 +0700

    commit in Debug room

--- src/main/java/StartApp.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Phornrawin on 27/8/2560.
- */
-public class StartApp {
-
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/controller/MainController.java
@@ -0,0 +1,7 @@
+package controller;
+
+/**
+ * Created by Phornrawin on 28/8/2560.
+ */
+public class MainController {
+}
